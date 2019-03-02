ea8903deb0b8cc230792a40e57e2dce0d1b29d91
jim <1160899400@qq.com>
2019-03-02 13:07:52 +0800

    添加对Manifest加入渠道信息

--- plugin/src/main/groovy/com/royole/plugin/helper/AndroidManifestHelper.groovy
+++ plugin/src/main/groovy/com/royole/plugin/helper/AndroidManifestHelper.groovy
@@ -1,4 +1,33 @@
 package com.royole.plugin.helper
 
+import groovy.xml.XmlUtil
+
 class AndroidManifestHelper {
+
+    static final String CHANNEL_META_DATA = "Channel"
+
+    File manifest
+
+    Node root
+
+    AndroidManifestHelper(File manifestFile){
+        XmlParser xmlParser = new XmlParser()
+        root = xmlParser.parse(manifestFile)
+        manifest = manifestFile
+    }
+
+
+    void addOrUpdateChannelMetaData(String channel){
+        def applicationNode = manifest.application[0]
+        List<Node> metaDatas = applicationNode.'meta-data'
+
+    }
+
+
+    /**
+     * 将内存中对manifest的修改写入文件
+     */
+    void save(){
+        manifest.write(XmlUtil.serialize(root))
+    }
 }
